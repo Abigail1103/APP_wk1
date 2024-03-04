1108c6b510d43039e7743499be26bf91a4c34fa0
Alva <xshinoa0208@gmail.com>
2024-03-04 17:06:38 +0800

    Initial commit

---
+++ wk1/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>110807002 林詩雅 -wk1 專案執行</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
